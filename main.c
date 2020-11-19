072bc4f16c0cb1d1f87916a018f9d2376bf529de
jsb9607 <57170950+jsb9607@users.noreply.github.com>
2020-11-19 16:36:23 +0900

    12_final

--- main.c
+++ main.c
@@ -5,22 +5,53 @@
 
 int main(int argc, char *argv[]) {
 
-	FILE* fp;
-	char str[100];
+	#if 0
+	FILE *fp = NULL;
+	char c;
 
-	fp = fopen("sample.txt", "w");
-
-	if (fp == NULL)	
+	fp = fopen("sample.txt", "r");	
+	if(fp == NULL)
 	{
 		return -1;
 	}
-
-	for(int i=0 ; i<3; i++)
+	
+	while((c = fgetc(fp)) != EOF)
 	{
-		printf("Input a word : ");
-		scanf("%s", str);
-		fprintf(fp, "%s\n", str);
+		printf("%c", c);
 	}
+
+	fclose(fp);
+	#endif
+
+	FILE *fp;
+	char str[100];
+	char path[100];
+	char lookup[100];
+
+	printf("input file path : ");
+	scanf("%s", path);	
+	printf("input search word : ");
+	scanf("%s", lookup);
+	
+
+	fp = fopen(path, "r");	
+	if(fp == NULL)
+	{
+		printf("invalid path! (%s)\n", path);
+		return -1;
+	}
+	
+	while(fgets(str, 100, fp) != NULL)
+	{	
+		if(strncmp(str, lookup, strlen(lookup)) == 0)
+		{
+			printf("Search succeed!\n");
+			fclose(fp);
+			return 0;
+		}
+	}
+
+	printf("Search failed!\n");
 	fclose(fp);
 	
 	return 0;
